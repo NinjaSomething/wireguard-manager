e70e73ee863b2cdbe3c306761ae6cc7c646e9b1e
James Carlson <james.carlson@energytoolbase.com>
2025-07-28 13:12:19 -0600

    Add documentation for the connection model parameters

--- src/models/connection.py
+++ src/models/connection.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
 from models.ssh import SshConnectionModel, SshConnectionResponseModel
 
@@ -19,8 +19,14 @@ class ConnectionModel(BaseModel):
     This model includes all the information required to connect to the wireguard server.  The data field is generic.
     """
 
-    type: ConnectionType
-    data: SshConnectionModel
+    type: ConnectionType = Field(
+        ...,
+        description="This is used to determine how the wireguard manager will communicate with the wireguard server.  "
+        "This is used to add and remove peers.",
+    )
+    data: SshConnectionModel = Field(
+        ..., description="These are the connection details for how to connect to the wireguard server. "
+    )
 
 
 class ConnectionResponseModel(BaseModel):
@@ -28,8 +34,14 @@ class ConnectionResponseModel(BaseModel):
     This model includes all the information required to connect to the wireguard server.  The data field is generic.
     """
 
-    type: ConnectionType
-    data: SshConnectionResponseModel
+    type: ConnectionType = Field(
+        ...,
+        description="This is used to determine how the wireguard manager will communicate with the wireguard server.  "
+        "This is used to add and remove peers.",
+    )
+    data: SshConnectionResponseModel = Field(
+        ..., description="These are the connection details for how to connect to the wireguard server. "
+    )
 
 
 def build_connection_model(connection_info: dict | None) -> ConnectionModel | None:
